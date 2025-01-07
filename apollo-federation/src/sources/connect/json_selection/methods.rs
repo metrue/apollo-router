d3b837d39495e09acd41a33462c8deed4d0025f2
Lenny Burdette <lenny@apollographql.com>
2025-01-07 14:52:12 -0500

    `->jsonStringify` method
    with this method, users can reimplement previous accidental behavior:
    
    ```graphql
    type Query {
      f(foos: [Int]): T
        @connect(http: { GET: "/{$args.foos}" }, selection: "$")
    }
    ```
    
    Prior to preview.3, this would result in `"/[1,2,3]"`. We removed that behavior so we could implement repeated path/query params in a principled way. But now you can achieve the same result with:
    
    ```graphql
    type Query {
      f(foos: [Int]): T
        @connect(http: { GET: "/{$args.foos->jsonStringify}" }, selection: "$")
    }
    ```

--- apollo-federation/src/sources/connect/json_selection/methods.rs
+++ apollo-federation/src/sources/connect/json_selection/methods.rs
@@ -34,6 +34,7 @@ pub(super) enum ArrowMethod {
     Slice,
     Size,
     Entries,
+    JsonStringify,
 
     // Future methods:
     TypeOf,
@@ -144,6 +145,7 @@ impl std::ops::Deref for ArrowMethod {
             Self::Slice => &public::SliceMethod,
             Self::Size => &public::SizeMethod,
             Self::Entries => &public::EntriesMethod,
+            Self::JsonStringify => &public::JsonStringifyMethod,
 
             // Future methods:
             Self::TypeOf => &future::TypeOfMethod,
@@ -199,6 +201,7 @@ impl ArrowMethod {
             "not" => Some(Self::Not),
             "or" => Some(Self::Or),
             "and" => Some(Self::And),
+            "jsonStringify" => Some(Self::JsonStringify),
             _ => None,
         };
 
@@ -222,6 +225,7 @@ impl ArrowMethod {
                 | Self::Slice
                 | Self::Size
                 | Self::Entries
+                | Self::JsonStringify
         )
     }
 }
